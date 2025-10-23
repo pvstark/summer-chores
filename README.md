110acdc13a18278bfc4d51decf6418bab8d1d889
Petra Stark <petravstark@gmail.com>
2025-10-23 12:46:22 -0400

    Adjusted use of Math.random to improve code readability and modularity

---
+++ README.md
@@ -0,0 +1,9 @@
+# Summer Chores
+---
+## Description
+Demonstrate knowledge of asynchronous JavaScript techniques including callbacks, promises, and async/await.
+The program simulates a series of summer chores by waiting a certain amount of time before each chore is complete.
+A random number is generated to determine if the chores will be complete or not.
+
+## Instructions/Usage
+Live Server can be used to open the program, at which point the JavaScript will run automatically and can be checked by viewing the console.
--- callbackVersion.js
+++ callbackVersion.js
@@ -7,69 +7,65 @@ function mowYard(name, callback) {
 }
 
 function weedEat(name, callback) {    
-        setTimeout(() => {
-        console.log(name +" ate the weeds.");
-        callback();
+    setTimeout(() => {
+        if(Math.random() < 0.9){
+            console.log(name +" ate the weeds.");
+            callback();
+        } else {
+            console.log(name + " fell asleep after mowing the yard.");
+        }
+        
     }, 1500);
 }
 
 function trimHedges(name, callback){
-        setTimeout(() => {
-        console.log(name +" trimmed the hedges.");
-        callback();
+    setTimeout(() => {
+        if(Math.random() < 0.8){
+            console.log(name +" trimmed the hedges.");
+            callback();
+        } else {
+            console.log(name + " fell asleep after eating the weeds.");
+        }
+        
     }, 1000);
 }
 
 function collectWood(name, callback){
-        setTimeout(() => {
-        console.log(name +" collected wood.");
-        callback();
+    setTimeout(() => {
+        if(Math.random() < 0.7){
+            console.log(name +" collected wood.");
+            callback();
+        } else {
+            console.log(name + " fell asleep after trimming the hedges.");
+        }
+        
     }, 2500);
 }
 
 function waterGarden(name){
     setTimeout(() => {
-        console.log(name +" watered the garden.");
-        console.log(name + " finished all of their chores!");
+        if(Math.random() < 0.6){
+            console.log(name +" watered the garden.");
+            console.log(name + " completed all of their chores!");
+        } else {
+            console.log(name + " fell asleep after collecting wood.");
+        }
     }, 500);
 }
 
 function doSummerChores(name){
 
-    //caffeine value of 16500 means that there is a 2/3 chance of completing all of the chores
-    //caffeine value is subtracted by the time in milliseconds it takes to complete each task after mowing the lawn
-    let caffeine = Math.floor((Math.random() * 16500))
-
     mowYard(name, () => {
-        caffeine -= 1500;
-        if(caffeine > 0){
-            weedEat(name, () => {
-                caffeine -= 1000;
-                if(caffeine > 0){
-                    trimHedges(name, () => {
-                        caffeine -= 2500;
-                        if(caffeine > 0){
-                            collectWood(name, () => {
-                                caffeine -= 500;
-                                if(caffeine > 0){
-                                waterGarden(name);
-                                } else {
-                                    console.log(name + " fell asleep after collecting wood.");
-                                }
-                            })
-                        } else {
-                            console.log(name + " fell asleep after trimming the hedges.");
-                        }
-                    })
-                } else {
-                    console.log(name + " fell asleep after eating the weeds.");
-                }
-            })
-        } else {
-            console.log(name + " fell asleep after mowing the yard.");
-        }
-    })
+        weedEat(name, () => {
+            trimHedges(name, () => {
+                collectWood(name, () => {
+                    waterGarden(name);
+                });
+            });
+        });
+    });
 
 }
+
 
 doSummerChores("Jane Doe");
